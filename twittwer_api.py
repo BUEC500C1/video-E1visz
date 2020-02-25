fa30729e8698240ef05a8f564d12bca70d48db18
E1visz <55214592+E1visz@users.noreply.github.com>
2020-02-24 19:22:57 -0500

    Update twittwer_api.py

--- twittwer_api.py
+++ twittwer_api.py
@@ -3,19 +3,30 @@ import keys
 # Imports the tweepy library
 import tweepy
 
+import json
 
-auth = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
-auth.set_access_token(keys.access_token, keys.access_token_secret)
 
-api = tweepy.API(auth)
 
 def Get_twitter(hashtag): #return the text message in a list
     #get the hashtages or handles for the function
     tag = hashtag + ' -filter:retweets'
 
     text = []
+
+    if keys.consumer_key == '':
+      jsonfile = open('example.json','r')
+      data = json.load(jsonfile)
+      text.append(data['user']['text'])
+
+    else:
+      auth = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
+      auth.set_access_token(keys.access_token, keys.access_token_secret)
+
+      api = tweepy.API(auth)
+
+
     #search the entire tweets in English, filter out the retweets. the number of the tweets is 10.
-    for tweet in tweepy.Cursor(api.search, q=tag, tweet_mode='extended', lang='en').items(10):
+      for tweet in tweepy.Cursor(api.search, q=tag, tweet_mode='extended', lang='en').items(10):
     # add the texts to the list
-        text.append(tweet.full_text)
+          text.append(tweet.full_text)
     return text
