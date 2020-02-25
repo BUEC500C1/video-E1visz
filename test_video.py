075bf538da394070b632cbd581f95cd01a85e0c8
E1visz <55214592+E1visz@users.noreply.github.com>
2020-02-24 19:30:46 -0500

    Update test_video.py

--- test_video.py
+++ test_video.py
@@ -1,5 +1,12 @@
 from twittwer_api import Get_twitter
+from textToImage import setAllImg
+from imgToVideo import imgToVideo
+import os
 
 def test_tweetApi():
   hashtag = "Test"
-  assert Get_twitter(hashtag) != ''
+  setAllImg(hashtag)
+  imgToVideo(hashtag)
+
+
+  assert os.path.exists('Test.avi') == True
